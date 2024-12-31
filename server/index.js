442ade7b421e9874f286564db6edfee4fb45adee
DAULAT Singh <daulats526@gmail.com>
2024-12-31 10:27:46 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:["https://project-study-notion-nqsp.vercel.app/"],
+		origin:["https://project-study-notion-nqsp.vercel.app"],
 		credentials:true,
 	})
 )
